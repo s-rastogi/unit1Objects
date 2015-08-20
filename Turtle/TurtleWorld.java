9d53d8ada22749eea6422490075ce23e6232a243
s-rastogi <srastogi@NN123-HA-18.HS.sd203.org>
2015-08-20 15:08:01 -0500

    Starting using turtles
    it failed

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,22 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.backward();
+        turtle.turnLeft();
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(500);
+        turtle.turnLeft();
+        turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(300);
+        turtle.turnLeft();
         turtle.forward(50);
     }
 }
