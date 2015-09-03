24dc4c3f780e3ad95edbc0bf060a52f01c1e9578
s-rastogi <srastogi@NN123-HA-18.HS.sd203.org>
2015-09-03 14:56:44 -0500

    final lab
    mask

--- TurtleLab/TurtleWorld.java
+++ TurtleLab/TurtleWorld.java
@@ -2,8 +2,9 @@
 import java.awt.Color;
 
 public class TurtleWorld
-{
+{  
     public static void main(String args[])
+    throws InterruptedException
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
@@ -11,26 +12,32 @@ public class TurtleWorld
         Turtle turtle3 = new Turtle(turtleWorld);
         Turtle turtle4 = new Turtle(turtleWorld);
         turtle.setColor(Color.CYAN);
-        turtle.setPenColor(Color.BLUE);
-        turtle2.setPenColor(Color.MAGENTA);
+        turtle.setPenColor(Color.YELLOW);
+        turtle2.setPenColor(Color.RED);
         turtle3.setPenColor(Color.MAGENTA);
-        turtle4.setPenColor(Color.MAGENTA);
+        turtle4.setPenColor(Color.BLUE);
         turtle.setShellColor(Color.BLACK);
         turtle.penDown();
         turtle2.penDown();
-        turtle2.penDown();
-        turtle2.penDown();
+        turtle3.penDown();
+        turtle4.penDown();
         int x =1;
-        while (x<5000)
+        while (x<470)
         {
           
             turtle.forward(50);
             turtle.turn(60);
             turtle.forward(50);
             turtle2.forward(100);
-            turtle.forward(80);
             turtle2.turn(60);
-            
+            turtle2.forward(100);
+            turtle3.forward(50);
+            turtle3.turn(300);
+            turtle3.forward(50);
+            turtle4.forward(100);
+            turtle4.turn(300);
+            turtle4.forward(100);
+            Thread.sleep(10);
             x+=1;
             
         }
